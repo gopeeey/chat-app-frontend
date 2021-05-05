94f945b66f73609710bbb6206e49f18ae16e72ce
Samuel Gopeh <sammygopeh@gmail.com>
2021-05-05 03:48:49 +0100

    changed endpoint to glitch

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client';
 
-const URL = 'https://fierce-castle-22690.herokuapp.com/';
+const URL = 'https://garnet-silken-stage.glitch.me';
 const socket = io(URL, {autoConnect: false});
 
 socket.onAny((event, ...args) => {
